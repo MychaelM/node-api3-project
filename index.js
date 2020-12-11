d93de08c2ad1f95d8ef4bf5b23dae5285c96ebfa
Mychael M <mychaelbmenges@gmail.com>
2020-12-10 17:09:34 -0700

    added port variable for deployment to utilize environment port

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 
 const server = require('./server');
+const port = process.env.PORT || 8080
 
-server.listen(8080, () => {
+server.listen(port, () => {
   console.log(`Server listening at http://localhost:8080`);
 });
